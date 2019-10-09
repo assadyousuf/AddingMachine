736a2040464dc755d3b340feb4470dceb28c0f1a
Assad Yousuf <your@email.com>
2019-10-09 16:53:28 -0700

    first commit

---
+++ AddingMachine.java
@@ -0,0 +1,45 @@
+/***
+ * Assignment:2
+ * Author:Assad Yousuf
+ * StudentID:1212371223
+ * ClassID:70641
+ * 
+ * 
+ * 
+ * 
+ */
+
+package cse360assign2;
+import java.util.*; 
+
+
+
+public class AddingMachine {
+
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return total;
+	}
+	
+	public void add (int value) {
+	
+	}
+	
+	public void subtract (int value) {
+		
+	}
+		
+	public String toString () {
+		 return "";
+	}
+
+	public void clear() {
+	
+	}
+	
+}
